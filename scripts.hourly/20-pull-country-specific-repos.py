756a2dd53e0d20b1518f97228c1ad913016f31b8
Theo Julienne <theojulienne@github.com>
2020-03-25 09:41:28 +1100

    Switch to covidtracking dataset for USA confirmed cases.

--- scripts.hourly/20-pull-country-specific-repos.py
+++ scripts.hourly/20-pull-country-specific-repos.py
@@ -10,7 +10,6 @@ country_specific_repos = {
   'USA': {
     'repo': 'theojulienne/covid-19-data-usa',
     'states': ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'],
-    'skip_totals': ['confirmed'], # don't import confirmed for now until numbers match JHU data
   }
 }
 
