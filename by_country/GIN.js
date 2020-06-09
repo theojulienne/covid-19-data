351c5cea14f1374da6106b13c5d6e88c2c2f82c1
GitHub Action <action@github.com>
2020-06-09 21:09:01 +0000

    Add latest dataset

--- by_country/GIN.js
+++ by_country/GIN.js
@@ -282,7 +282,8 @@ var covid19_dataset_country_gin = {
       4060,
       4117,
       4117,
-      4216
+      4216,
+      4258
     ],
     "deaths": [
       0,
@@ -564,7 +565,8 @@ var covid19_dataset_country_gin = {
       2667,
       2857,
       2877,
-      2918
+      2918,
+      2942
     ]
   }
 };
