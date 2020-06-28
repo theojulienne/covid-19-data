a4ff140f7c7f6fa3fc43e30caab579d730ad7ac3
GitHub Action <action@github.com>
2020-06-28 04:10:00 +0000

    Add latest dataset

--- dataset_latest_totals.js
+++ dataset_latest_totals.js
@@ -193,7 +193,7 @@ var covid19_dataset_latest_totals = {
           "current_icu": 0,
           "deaths": 6,
           "recovered": 1054,
-          "tested": 274688
+          "tested": 353085
         },
         "SA": {
           "confirmed": 440,
