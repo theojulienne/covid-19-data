a4687a5773a27a32d53ebe3593c214f3f90210d2
GitHub Action <action@github.com>
2020-07-25 11:11:18 +0000

    Add latest dataset

--- dataset_world_totals.js
+++ dataset_world_totals.js
@@ -375,7 +375,7 @@ var covid19_dataset_world_totals = {
       15166507,
       15450671,
       15676200,
-      15686805
+      15690803
     ],
     "deaths": [
       17,
@@ -563,7 +563,7 @@ var covid19_dataset_world_totals = {
       615159,
       625044,
       631230,
-      631443
+      631653
     ],
     "recovered": [
       28,
@@ -751,7 +751,7 @@ var covid19_dataset_world_totals = {
       8627609,
       8796840,
       9025843,
-      9032657
+      9035540
     ]
   }
 };
