0d972519639abcd6ee5d83e8fac19487a0120bca
GitHub Action <action@github.com>
2020-08-11 00:37:29 +0000

    Add latest dataset

--- dataset_top10.js
+++ dataset_top10.js
@@ -6547,7 +6547,8 @@ var covid19_dataset_top10 = {
     "2020-08-08",
     "2020-08-09",
     "2020-08-10",
-    "2020-08-11"
+    "2020-08-11",
+    "2020-08-12"
   ]
 };
 if (covid19_dataset_callback) covid19_dataset_callback('covid19_dataset_top10', covid19_dataset_top10, {});
