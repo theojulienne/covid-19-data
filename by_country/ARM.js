9ff6f7c57e0e5ba6a607d9f7e1df6c1aa9e55c24
GitHub Action <action@github.com>
2020-08-20 08:15:34 +0000

    Add latest dataset

--- by_country/ARM.js
+++ by_country/ARM.js
@@ -426,7 +426,8 @@ var covid19_dataset_country_arm = {
       41663,
       41701,
       41846,
-      42056
+      42056,
+      42319
     ],
     "deaths": [
       0,
@@ -639,7 +640,8 @@ var covid19_dataset_country_arm = {
       818,
       824,
       832,
-      833
+      833,
+      836
     ],
     "recovered": [
       0,
@@ -852,7 +854,8 @@ var covid19_dataset_country_arm = {
       34584,
       34655,
       34982,
-      35226
+      35226,
+      35476
     ]
   }
 };
