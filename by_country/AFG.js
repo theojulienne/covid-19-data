f147e8f5b59586ba6d2bf9669f492ca2c91c19cd
GitHub Action <action@github.com>
2020-10-12 08:21:16 +0000

    Add latest dataset

--- by_country/AFG.js
+++ by_country/AFG.js
@@ -532,7 +532,8 @@ var covid19_dataset_country_afg = {
       39616,
       39693,
       39703,
-      39799
+      39799,
+      39870
     ],
     "deaths": [
       0,
@@ -798,7 +799,8 @@ var covid19_dataset_country_afg = {
       1470,
       1472,
       1473,
-      1477
+      1477,
+      1479
     ],
     "recovered": [
       0,
@@ -1064,7 +1066,8 @@ var covid19_dataset_country_afg = {
       33058,
       33058,
       33064,
-      33114
+      33114,
+      33118
     ]
   }
 };
