8ecf109d580ea0c0d58ad979095800549f22f0e4
GitHub Action <action@github.com>
2020-10-25 23:19:36 +0000

    Add latest dataset

--- by_country/CAN.js
+++ by_country/CAN.js
@@ -5338,7 +5338,7 @@ var covid19_dataset_country_can = {
           70270,
           71140,
           71423,
-          73024
+          73026
         ],
         "deaths": [
           0,
@@ -8433,7 +8433,7 @@ var covid19_dataset_country_can = {
       211617,
       214519,
       216051,
-      218531
+      218533
     ],
     "deaths": [
       0,
