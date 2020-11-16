3cbab0820c52549f0e87c5b6a3f975f7b8d2541a
GitHub Action <action@github.com>
2020-11-16 18:15:14 +0000

    Add latest dataset

--- by_country/CZE.js
+++ by_country/CZE.js
@@ -602,7 +602,8 @@ var covid19_dataset_country_cze = {
       446675,
       454030,
       458229,
-      460116
+      460116,
+      462121
     ],
     "deaths": [
       0,
@@ -903,7 +904,8 @@ var covid19_dataset_country_cze = {
       5755,
       5926,
       6058,
-      6208
+      6208,
+      6370
     ],
     "recovered": [
       0,
@@ -1204,7 +1206,8 @@ var covid19_dataset_country_cze = {
       300719,
       316352,
       322383,
-      327306
+      327306,
+      328019
     ]
   }
 };
