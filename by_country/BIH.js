a216f5df7a9a9544b00cd9af6bcd0fec0ac16c56
GitHub Action <action@github.com>
2020-11-21 16:22:31 +0000

    Add latest dataset

--- by_country/BIH.js
+++ by_country/BIH.js
@@ -612,7 +612,8 @@ var covid19_dataset_country_bih = {
       73944,
       75577,
       76757,
-      77994
+      77994,
+      79309
     ],
     "deaths": [
       0,
@@ -918,7 +919,8 @@ var covid19_dataset_country_bih = {
       2042,
       2089,
       2153,
-      2209
+      2209,
+      2246
     ],
     "recovered": [
       0,
@@ -1224,7 +1226,8 @@ var covid19_dataset_country_bih = {
       38660,
       39886,
       41031,
-      42916
+      42916,
+      43793
     ]
   }
 };
