bbd4d07c39a7c1dcd36a30de0a940f30866076da
GitHub Action <action@github.com>
2020-12-29 19:37:15 +0000

    Add latest dataset

--- by_country/IRL.js
+++ by_country/IRL.js
@@ -688,7 +688,8 @@ var covid19_dataset_country_irl = {
       84098,
       85394,
       86129,
-      86894
+      86894,
+      88439
     ],
     "deaths": [
       0,
@@ -1032,7 +1033,8 @@ var covid19_dataset_country_irl = {
       2194,
       2200,
       2204,
-      2205
+      2205,
+      2213
     ],
     "recovered": [
       0,
