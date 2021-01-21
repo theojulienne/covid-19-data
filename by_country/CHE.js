2d6621553571531a1095330c4e1b2d798bd18892
GitHub Action <action@github.com>
2021-01-21 13:59:00 +0000

    Add latest dataset

--- by_country/CHE.js
+++ by_country/CHE.js
@@ -734,7 +734,8 @@ var covid19_dataset_country_che = {
       495228,
       499931,
       502191,
-      504918
+      504918,
+      507123
     ],
     "deaths": [
       0,
@@ -1101,7 +1102,8 @@ var covid19_dataset_country_che = {
       8682,
       8792,
       8859,
-      8924
+      8924,
+      8938
     ],
     "recovered": [
       0,
