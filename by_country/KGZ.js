9501b67d9d3ef8dbc0ef5ee4ae85d1739b520a71
GitHub Action <action@github.com>
2021-02-07 07:18:21 +0000

    Add latest dataset

--- by_country/KGZ.js
+++ by_country/KGZ.js
@@ -768,7 +768,8 @@ var covid19_dataset_country_kgz = {
       84832,
       84920,
       84920,
-      84992
+      84992,
+      85071
     ],
     "deaths": [
       0,
@@ -1152,7 +1153,8 @@ var covid19_dataset_country_kgz = {
       1418,
       1420,
       1420,
-      1423
+      1423,
+      1426
     ],
     "recovered": [
       0,
@@ -1536,7 +1538,8 @@ var covid19_dataset_country_kgz = {
       81334,
       81444,
       81444,
-      81542
+      81542,
+      81635
     ]
   }
 };
