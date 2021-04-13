30526c1f1e08389c835d8e9c8ebf50b15eac5a80
GitHub Action <action@github.com>
2021-04-13 13:12:22 +0000

    Add latest dataset

--- by_country/RKS.js
+++ by_country/RKS.js
@@ -898,7 +898,8 @@ var covid19_dataset_country_rks = {
       95886,
       95886,
       95886,
-      95886
+      95886,
+      98015
     ],
     "deaths": [
       0,
@@ -1347,7 +1348,8 @@ var covid19_dataset_country_rks = {
       1968,
       1968,
       1968,
-      1968
+      1968,
+      2004
     ],
     "recovered": [
       0,
@@ -1796,7 +1798,8 @@ var covid19_dataset_country_rks = {
       80344,
       80344,
       80344,
-      80344
+      80344,
+      82794
     ]
   }
 };
