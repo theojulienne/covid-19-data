b4f90dc897bef8ad990a7ce8ff16fe65d7a21896
GitHub Action <action@github.com>
2021-06-07 00:17:11 +0000

    Add latest dataset

--- dataset_top10_by_deaths.js
+++ dataset_top10_by_deaths.js
@@ -16050,7 +16050,8 @@ var covid19_dataset_top10_by_deaths = {
     "2021-06-04",
     "2021-06-05",
     "2021-06-06",
-    "2021-06-07"
+    "2021-06-07",
+    "2021-06-08"
   ]
 };
 if (covid19_dataset_callback) covid19_dataset_callback('covid19_dataset_top10_by_deaths', covid19_dataset_top10_by_deaths, {});
