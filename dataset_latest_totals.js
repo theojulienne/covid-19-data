b4ae75ad29ec567515d7b58748ebb6c8b9bb7e94
GitHub Action <action@github.com>
2021-06-08 21:05:25 +0000

    Add latest dataset

--- dataset_latest_totals.js
+++ dataset_latest_totals.js
@@ -3320,19 +3320,19 @@ var covid19_dataset_latest_totals = {
     },
     "TUN": {
       "back_1week": {
-        "confirmed": 344688,
-        "deaths": 12623,
-        "recovered": 301869
+        "confirmed": 345474,
+        "deaths": 12654,
+        "recovered": 303467
       },
       "back_2week": {
-        "confirmed": 335345,
-        "deaths": 12236,
-        "recovered": 295789
+        "confirmed": 336414,
+        "deaths": 12273,
+        "recovered": 297123
       },
       "latest": {
-        "confirmed": 355732,
-        "deaths": 12980,
-        "recovered": 311340
+        "confirmed": 356750,
+        "deaths": 13027,
+        "recovered": 312979
       }
     },
     "TUR": {
