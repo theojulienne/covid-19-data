a7122e8c17929f5d21e8cc33cc1ebe0e04d0f443
GitHub Action <action@github.com>
2021-12-21 10:05:53 +0000

    Add latest dataset

--- by_country/EST.js
+++ by_country/EST.js
@@ -1402,7 +1402,8 @@ var covid19_dataset_country_est = {
       231117,
       231694,
       232120,
-      232506
+      232506,
+      233367
     ],
     "deaths": [
       0,
@@ -2103,7 +2104,8 @@ var covid19_dataset_country_est = {
       1874,
       1882,
       1884,
-      1888
+      1888,
+      1893
     ],
     "recovered": [
       0,
