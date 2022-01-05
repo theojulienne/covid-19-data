8d07616c8f0230171d4ad5950e205c230c29ef0b
GitHub Action <action@github.com>
2022-01-05 18:05:41 +0000

    Add latest dataset

--- by_country/MKD.js
+++ by_country/MKD.js
@@ -1432,7 +1432,8 @@ var covid19_dataset_country_mkd = {
       225484,
       225730,
       225972,
-      227168
+      227168,
+      228540
     ],
     "deaths": [
       0,
@@ -2148,7 +2149,8 @@ var covid19_dataset_country_mkd = {
       7969,
       7975,
       7981,
-      7996
+      7996,
+      8000
     ],
     "recovered": [
       0,
