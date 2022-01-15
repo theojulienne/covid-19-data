75dba6554d0c34e94a495d2e7a3e9e565ef39e27
GitHub Action <action@github.com>
2022-01-15 00:12:47 +0000

    Add latest dataset

--- dataset_top10.js
+++ dataset_top10.js
@@ -22931,7 +22931,8 @@ var covid19_dataset_top10 = {
     "2022-01-12",
     "2022-01-13",
     "2022-01-14",
-    "2022-01-15"
+    "2022-01-15",
+    "2022-01-16"
   ]
 };
 if (covid19_dataset_callback) covid19_dataset_callback('covid19_dataset_top10', covid19_dataset_top10, {});
--- dataset_top10_by_deaths.js
+++ dataset_top10_by_deaths.js
@@ -22933,7 +22933,8 @@ var covid19_dataset_top10_by_deaths = {
     "2022-01-12",
     "2022-01-13",
     "2022-01-14",
-    "2022-01-15"
+    "2022-01-15",
+    "2022-01-16"
   ]
 };
 if (covid19_dataset_callback) covid19_dataset_callback('covid19_dataset_top10_by_deaths', covid19_dataset_top10_by_deaths, {});
