c2c7747d2025aa69f7ee679eac51ce701a8e8f58
GitHub Action <action@github.com>
2022-01-18 07:05:51 +0000

    Add latest dataset

--- by_country/BOL.js
+++ by_country/BOL.js
@@ -1458,7 +1458,8 @@ var covid19_dataset_country_bol = {
       725459,
       725459,
       749070,
-      754335
+      754335,
+      763392
     ],
     "deaths": [
       0,
@@ -2187,7 +2188,8 @@ var covid19_dataset_country_bol = {
       20149,
       20149,
       20238,
-      20254
+      20254,
+      20291
     ],
     "recovered": [
       0,
