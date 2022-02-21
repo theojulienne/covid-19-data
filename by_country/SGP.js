66212d39a17d7ea7dd41ae863f9ffd87729c6865
GitHub Action <action@github.com>
2022-02-21 17:05:50 +0000

    Add latest dataset

--- by_country/SGP.js
+++ by_country/SGP.js
@@ -1526,7 +1526,8 @@ var covid19_dataset_country_sgp = {
       533425,
       551519,
       576355,
-      582638
+      582638,
+      596261
     ],
     "deaths": [
       0,
@@ -2289,7 +2290,8 @@ var covid19_dataset_country_sgp = {
       930,
       937,
       941,
-      945
+      945,
+      952
     ],
     "recovered": [
       0,
