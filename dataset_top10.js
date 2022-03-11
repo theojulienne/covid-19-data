17cb292bc86d4cfc485d5fad613f34d514a90dc7
GitHub Action <action@github.com>
2022-03-11 00:18:57 +0000

    Add latest dataset

--- dataset_top10.js
+++ dataset_top10.js
@@ -24638,7 +24638,8 @@ var covid19_dataset_top10 = {
     "2022-03-08",
     "2022-03-09",
     "2022-03-10",
-    "2022-03-11"
+    "2022-03-11",
+    "2022-03-12"
   ]
 };
 if (covid19_dataset_callback) covid19_dataset_callback('covid19_dataset_top10', covid19_dataset_top10, {});
