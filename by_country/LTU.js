4441be52edb76b7bff83b6753419bb8e5804fd70
GitHub Action <action@github.com>
2022-03-21 09:05:46 +0000

    Add latest dataset

--- by_country/LTU.js
+++ by_country/LTU.js
@@ -1582,7 +1582,8 @@ var covid19_dataset_country_ltu = {
       980331,
       984980,
       989430,
-      992101
+      992101,
+      993953
     ],
     "deaths": [
       0,
@@ -2373,7 +2374,8 @@ var covid19_dataset_country_ltu = {
       8692,
       8704,
       8716,
-      8728
+      8728,
+      8736
     ],
     "recovered": [
       0,
