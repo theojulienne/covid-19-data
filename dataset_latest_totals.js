a2b7c6bf02b86c87f2af57a0861c044e1c864d8a
GitHub Action <action@github.com>
2022-03-24 03:10:48 +0000

    Add latest dataset

--- dataset_latest_totals.js
+++ dataset_latest_totals.js
@@ -160,7 +160,7 @@ var covid19_dataset_latest_totals = {
         "current_icu": 8,
         "deaths": 35,
         "recovered": 19522,
-        "tested": 4871782
+        "tested": 4874805
       },
       "back_2week": {
         "confirmed": 44985,
@@ -168,7 +168,7 @@ var covid19_dataset_latest_totals = {
         "current_icu": 8,
         "deaths": 35,
         "recovered": 19522,
-        "tested": 4872260
+        "tested": 4875619
       },
       "latest": {
         "confirmed": 50660,
@@ -176,7 +176,7 @@ var covid19_dataset_latest_totals = {
         "current_icu": 8,
         "deaths": 35,
         "recovered": 19522,
-        "tested": 4874822
+        "tested": 4880500
       },
       "states": {
         "ACT": {
@@ -210,7 +210,7 @@ var covid19_dataset_latest_totals = {
           "current_icu": 0,
           "deaths": 6,
           "recovered": 0,
-          "tested": 7003
+          "tested": 12681
         },
         "SA": {
           "confirmed": 871,
